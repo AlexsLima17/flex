9be09e4c6d4cb63940a2d0b36fa88c039c73e9e5
ALEX SANTOS LIMA <alex.slima13@senacsp.edu.br>
2025-02-12 19:23:03 -0300

    12/02

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 /**
  * APP PWA - Etanol x Gasolina
- * @author Alex Lima
- * @Version 2.0
+ * @author Alex
+ * @version 2.0
  */
 
+//===========================================
+//Registro do service worker
+
+// se o navegador de internet suportar este recurso
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => {
+            console.log("Service worker registrado!")
+        })
+}
+//===========================================
+
 function calcular() {
-    // caoturar os valores das caixas de input
-        let gasolina = document.getElementById('gasolina').value
-        let etanol = document.getElementById('etanol').value
-        let kmlGasolina = document.getElementById('kmGasolina').value
-        let kmlEtanol = document.getElementById('kmEtanol').value
-       //calculo da vantagem
-       if (etanol < (kmlEtanol / kmlGasolina) * gasolina) {
+    //capturar os valores das caixas de input
+    let gasolina = document.getElementById('gasolina').value
+    let etanol = document.getElementById('etanol').value
+    let kmlGasolina = document.getElementById('kmGasolina').value
+    let kmlEtanol = document.getElementById('kmEtanol').value
+    //cálculo da vantagem
+    if (etanol < (kmlEtanol / kmlGasolina) * gasolina) {
         document.getElementById('status').src = "img/etanol.png"
-       } else {
-        document.getElementById('status').src = "img/gasolina.png"
-       }
-    }
-    //cálculo de vantagem
-    if(etanol < (Etanol / kmlGasolina) * gasolina ){
-        document.getElementById('status').src = "img/etanol.png"
-    }else{
+    } else {
         document.getElementById('status').src = "img/gasolina.png"
     }
-
-
+}
